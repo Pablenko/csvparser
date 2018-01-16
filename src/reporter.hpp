29bb5874f27903d2ab65177d8644d470c28b4a62
Pawel Przybylski <pawel.przybylski91@gmail.com>
2018-01-17 00:48:18 +0100

    Corrected typo.

--- src/reporter.hpp
+++ src/reporter.hpp
@@ -21,7 +21,7 @@ class console_reporter : public reporter
     virtual void report(float new_ammount, const std::vector<std::pair<std::string, float>>& summary)
     {
         std::cout << "Report: \n";
-        std::cout << "   New amount of money: " << std::fixed <<std::setprecision(2) << new_ammount << "\n";
+        std::cout << "   New ammount of money: " << std::fixed <<std::setprecision(2) << new_ammount << "\n";
         std::cout << "   Accounts balance: \n";
         for(const auto& elem : summary)
         {
